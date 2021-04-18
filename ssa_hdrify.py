2c955e843e4fa28a4d9306e37a974e4ae79ace16
ying <yingyuyao+yyymeow@gmail.com>
2021-04-17 18:13:02 -0700

    add CLI, add workflow

--- ssa_hdrify.py
+++ ssa_hdrify.py
@@ -1,5 +1,8 @@
+"""调整SSA字幕亮度。"""
+
 from __future__ import annotations
 
+import argparse
 import os
 import re
 from tkinter import Tk, filedialog
@@ -7,38 +10,48 @@ from tkinter import Tk, filedialog
 import ass as ssa
 import colour
 import numpy as np
-from colour.models import RGB_COLOURSPACE_BT2020, RGB_COLOURSPACE_sRGB
+from colour.models import (RGB_COLOURSPACE_BT2020, RGB_COLOURSPACE_DCI_P3,
+                           RGB_COLOURSPACE_sRGB)
 
-SRGB_PEAK_BRIGHTNESS = 120
-SCREEN_PEAK_BRIGHTNESS = 1000
 D65_ILLUMINANT = RGB_COLOURSPACE_sRGB.whitepoint
 
 
-def file_picker() -> str:
-    """询问用户并返回一个文件夹"""
+def files_picker() -> list[str]:
+    """询问用户并返回字幕文件"""
     root = Tk()
     root.withdraw()
-    return filedialog.askopenfilename()
+    return filedialog.askopenfilenames(filetypes=[('ASS files', '.ass .ssa'),
+                                                  ('all files', '.*')])
 
 
-def sRgbToBt2020(source: tuple[int, int, int]) -> tuple[int, int, int]:
+def sRgbToHdr(source: tuple[int, int, int]) -> tuple[int, int, int]:
     """
     大致思路：先做gamma correction，然后转入XYZ。 在xyY内将Y的极值由sRGB亮度调为输出
     亮度，然后转回2020的RGB。
     args:
     colour -- (0-255, 0-255, 0-255)
     """
+    if source == (0, 0, 0):
+        return (0, 0, 0)
+
+    args = parse_args()
+    srgb_brightness = args.sub_brightness
+    screen_brightness = args.output_brightness
+    target_colourspace = RGB_COLOURSPACE_BT2020
+    if args.colourspace == 'dcip3':
+        target_colourspace = RGB_COLOURSPACE_DCI_P3
+
     normalized_source = np.array(source) / 255
     linear_source = colour.oetf_inverse(normalized_source, 'ITU-R BT.709')
     xyz = colour.RGB_to_XYZ(linear_source, RGB_COLOURSPACE_sRGB.whitepoint,
                             D65_ILLUMINANT,
                             RGB_COLOURSPACE_sRGB.matrix_RGB_to_XYZ)
     xyy = colour.XYZ_to_xyY(xyz)
-    xyy[2] = xyy[2] * SRGB_PEAK_BRIGHTNESS / SCREEN_PEAK_BRIGHTNESS
+    xyy[2] = xyy[2] * srgb_brightness / screen_brightness
     xyz = colour.xyY_to_XYZ(xyy)
     output = colour.XYZ_to_RGB(xyz, D65_ILLUMINANT,
-                               RGB_COLOURSPACE_BT2020.whitepoint,
-                               RGB_COLOURSPACE_BT2020.matrix_XYZ_to_RGB)
+                               target_colourspace.whitepoint,
+                               target_colourspace.matrix_XYZ_to_RGB)
     output = colour.oetf(output, 'ITU-R BT.2100 PQ')
     output = np.trunc(output * 255)
     return (int(output[0]), int(output[1]), int(output[2]))
@@ -46,12 +59,10 @@ def sRgbToBt2020(source: tuple[int, int, int]) -> tuple[int, int, int]:
 
 def transformColour(colour):
     rgb = (colour.r, colour.g, colour.b)
-    transformed = sRgbToBt2020(rgb)
+    transformed = sRgbToHdr(rgb)
     colour.r = transformed[0]
     colour.g = transformed[1]
     colour.b = transformed[2]
-
-    print(f'process {rgb}, output {transformed}')
 
 
 def transformEvent(event):
@@ -65,7 +76,7 @@ def transformEvent(event):
         b = int(hex_colour[0:2], 16)
         g = int(hex_colour[2:4], 16)
         r = int(hex_colour[4:6], 16)
-        (r, g, b) = sRgbToBt2020((r, g, b))
+        (r, g, b) = sRgbToHdr((r, g, b))
         hex_colour = '{:02x}{:02x}{:02x}'.format(b, g, r)
         matches.append((start, end, hex_colour.upper()))
 
@@ -76,6 +87,10 @@ def transformEvent(event):
 
 
 def ssaProcessor(fname: str):
+    if not os.path.isfile(fname):
+        print(f'文件不存在: {fname}')
+        return
+
     with open(fname, encoding='utf_8_sig') as f:
         sub = ssa.parse(f)
     for s in sub.styles:
@@ -92,7 +107,54 @@ def ssaProcessor(fname: str):
 
     with open(output_fname, 'w', encoding='utf_8_sig') as f:
         sub.dump_file(f)
+        print(f'写入 {output_fname}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s',
+                        '--sub-brightness',
+                        metavar='val',
+                        type=int,
+                        help=("设置字幕最大亮度，纯白色字幕将被映射为该亮度。"
+                              "(默认: %(default)s)"),
+                        default=100)
+
+    parser.add_argument('-o',
+                        '--output-brightness',
+                        metavar='val',
+                        type=int,
+                        help=("设置输出屏幕最大亮度。"
+                              "(默认: %(default)s)"),
+                        default=1000)
+    parser.add_argument('-c',
+                        '--colourspace',
+                        metavar='{dcip3,bt2020}',
+                        type=str,
+                        help=('选择输出的色彩空间。可用值为 dcip3 和 bt2020。'
+                              '(默认: %(default)s)'),
+                        default='bt2020')
+
+    parser.add_argument('-f',
+                        '--file',
+                        metavar='path',
+                        type=str,
+                        help=('输入字幕文件。可重复添加。'),
+                        action='append')
+
+    args = parser.parse_args()
+
+    args.output_brightness
+
+    return args
 
 
 if __name__ == '__main__':
-    ssaProcessor(file_picker())
+    args = parse_args()
+    files = args.file
+    if not files:
+        files = files_picker()
+    for f in files:
+        ssaProcessor(f)
+
+    input("按回车键退出……...")
